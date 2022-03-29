1932a9e33a38695e540518092ff928210c806d07
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2022-03-29 12:32:39 +0000

    just creating the project

---
+++ product-catalogue-backend/src/main/java/me/obelmokhtar/productcataloguebackend/ProductCatalogueBackendApplication.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.productcataloguebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogueBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogueBackendApplication.class, args);
+    }
+
+}
---
+++ product-catalogue-backend/src/test/java/me/obelmokhtar/productcataloguebackend/ProductCatalogueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.productcataloguebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogueBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
